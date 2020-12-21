d29fb0a24d798b0d8af8ef0e257b4d79d5489d37
jjenkins120 <jjenkins120@gmail.com>
2020-12-21 11:55:58 -0600

    asyncstorage bug fixed

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'https://ea9a672987ec.ngrok.io'
+    baseURL:'http://5eac2de7d37f.ngrok.io'
 })
 //REMEMBER NGROK URL HAS TO BE UPDATED EVERY 8 HRS
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from '@react-native-async-storage/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
 import { navigate } from '../navigationRef'
