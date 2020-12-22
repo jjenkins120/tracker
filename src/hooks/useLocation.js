1cf3416b0c298102afb255caeff0af691be5f7ea
jjenkins120 <jjenkins120@gmail.com>
2020-12-22 17:27:53 -0600

    cleaned up callback issue

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -3,41 +3,55 @@ import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-loca
 
 export default (shouldTrack, callback) => {
     const [err, setErr] = useState(null)
-    const [subscriber, setSubscriber] = useState(null) 
     
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestPermissionsAsync();
-            const sub = await watchPositionAsync({
-              accuracy: Accuracy.BestForNavigation,
-              timeInterval: 1000,
-              distanceInterval: 10
-          }, callback)
-          //this denotes the accuracy we want our points to be. we also set it to get an update every second and every 10 meters. the second arg is a function that denotes the users location
-          //watchPositionAsync is essentially a background process that tracks the user location - in order for us turn off location tracking, we need to use state logic and useeffect with a boolean value
-          if (!granted) {
-              throw new Error('Location permission not granted');
-            }
-            setSubscriber(sub)
-        } catch (e) {
-            setErr(e);
-        }
-    };
-    //this function is used to asked permission from the user to track their location - requestPermissionsAsync essentially lets us do this prompt
-
+    
     useEffect(()=>{
+        let subscriber
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestPermissionsAsync();
+                subscriber = await watchPositionAsync({
+                  accuracy: Accuracy.BestForNavigation,
+                  timeInterval: 1000,
+                  distanceInterval: 10
+              }, callback)
+              //this denotes the accuracy we want our points to be. we also set it to get an update every second and every 10 meters. the second arg is a function that denotes the users location
+              //watchPositionAsync is essentially a background process that tracks the user location - in order for us turn off location tracking, we need to use state logic and useeffect with a boolean value
+              if (!granted) {
+                  throw new Error('Location permission not granted');
+                }
+            } catch (e) {
+                setErr(e);
+            }
+        };
+        //this function is used to asked permission from the user to track their location - requestPermissionsAsync essentially lets us do this prompt
         if (shouldTrack){
             startWatching()
         } else {
+            if (subscriber){
             subscriber.remove()
-            setSubscriber(null)
+            }
+            subscriber = null
         }
-    }, [shouldTrack])
+
+        return () => {
+            if (subscriber) {
+                subscriber.remove()
+            }
+        }
+
+    }, [shouldTrack, callback])
     //if shouldTrack changes, the useEffect hook is rerun. shouldTrack is expected to be a boolean value and, in this case, is the isFocused passed from the trackcreatescreen
     //subscriber.remove stops the back background tracking (sub/watchPositionAsync from running)
+    //the return function keeps startwatching from being called over and over
 
     return [err]
     //there is nothing special about returning an array, its just the convention for hooks. Because we need the err for the trackCreateScreen return statement, we are returning it here for use in the component that is importing it
 }
 
 //this custom hook will allow use to not run the addLocation while we are not on the map screen (because running it eats battery)
+
+//IMPORTANT TIPS & BEST PRACTICES FROM REACT DEVS ON USING USEEFFECT
+    //  1. if we reference props, state, or context in useeffect, they should also be in the dependency array
+    //  2. anytime we have a have helper function defined in our component and called in useeffect, the function should be defined in useeffect &
+    //  3. if the helper function has state/props/context referenced in there, they should be added to the dependency array
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation'
-import React, { useContext } from 'react'
+import React, { useContext, useCallback } from 'react'
 import useLocation from '../hooks/useLocation'
 import { StyleSheet, SafeAreaView } from 'react-native'
 import { withNavigationFocus } from 'react-navigation'
@@ -10,10 +10,11 @@ import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
     const { state, addLocation } = useContext(LocationContext)
-    
-    const [err] = useLocation(isFocused, location => addLocation(location, state.recording))
+    const callback = useCallback(location => addLocation(location, state.recording), [state.recording])
+    const [err] = useLocation(isFocused, callback)
     //we get access to err (from an array) because it is returned from the useLocation hook 
     //isFocused is the boolean that we will need to pass to the hook in order to stop the location tracking
+    //useCallback hook limits the number of times we create a new callback function (ran into some issues with this, see tutorial for explanation). This helps to resolve the callback issue with useEffect. As long as the value in the array(in this case state.recording) doesn't change, the callback will refer to the same point in memory. When it changes it refers to a new point in memory.
 
     return (
         <SafeAreaView>
